c4d59b82cff1122cd254ee1c79543e521f82b49c
Mustafa khan <94809609+Mustafa7Khan@users.noreply.github.com>
2024-07-30 23:21:17 +0000

    day 1 done some tasks

---
+++ day-1.js
@@ -0,0 +1,16 @@
+// VARIABLES AND DATATYPES
+
+// 01 : Variable Declaration
+
+var num = 5;
+console.log(num);
+
+let str = "hello world";
+console.log(str);
+
+// 02 : Constant Decleration
+
+const value = true;
+console.log(value);
+
+// 03 : Data Types
