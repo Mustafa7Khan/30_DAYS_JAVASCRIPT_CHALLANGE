6bd6795b8c201131231de9e9f8e121978212efe2
Mustafa7Khan <musa.khan5605@gmail.com>
2024-07-31 06:02:20 -0700

    day 1 done

--- day-1.js
+++ day-1.js
@@ -2,8 +2,8 @@
 
 // 01 : Variable Declaration
 
-var num = 5;
-console.log(num);
+var num1 = 5;
+console.log(num1);
 
 let str = "hello world";
 console.log(str);
@@ -14,3 +14,36 @@ const value = true;
 console.log(value);
 
 // 03 : Data Types
+
+
+var number = 5;
+console.log(typeof number);
+
+let str1 = "hello world";
+console.log(typeof str1);
+
+const value1 = true;
+console.log(typeof value1);
+
+const arr1 = [1,12,3,4,4];
+console.log(typeof arr1);
+
+const obj = {
+    name : "khan",
+    age : 14
+};
+console.log(typeof obj);
+
+// 04 : Reassigning Variables
+
+let num=  5;
+console.log(num);
+num =10;
+console.log(num);
+
+// 05 : Understanding const
+
+const name = "khan";
+console.log(name);
+// name = "zaib";
+// console.log(name);
