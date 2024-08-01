f657a0d03d1a51b99a373054af32ccbfbeede26c
Mustafa khan <94809609+Mustafa7Khan@users.noreply.github.com>
2024-08-02 00:39:02 +0500

    Add files via upload
    4 day done function

---
+++ day-4.js
@@ -0,0 +1,72 @@
+// LOOPS
+
+// 01 : For Loop
+// numbers from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//5 table
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`5 * ${i} = ${5 * i}`)
+// }
+
+// 02 : While Loop 
+// the sum of numbers from 1 to 10
+
+// let sum =0;
+// let i= 1;
+// while (i<=10) {
+//     sum +=i;
+//     i++;
+// }
+// console.log(sum); //55
+
+//Numbers from 10 to 1
+// let j= 10;
+// while (j>=1) {
+//     console.log(j)
+//     j--;
+// }
+
+// 03 : Do...While Loop
+//numbers from 1 to 5
+// let k = 1;
+// do {
+//     console.log(k)
+//     k++;
+// } while (k <= 5);
+
+//factorial
+// let m = 1;
+// let fact = 1;
+// let num = 5;
+// do {
+//     fact *= m;
+//     m++;
+// } while (m <= num);
+// console.log(fact);
+
+// 04 : Nested Loops
+
+// for (let i = 0; i <= 5; i++) {
+//     let pattern = "" ;
+//     for (let j = 0; j <= i; j++) {
+//         pattern += " * ";
+        
+//     };
+//     console.log(pattern);
+// };
+
+// 05 : Loop Control Statements
+
+// for (let i  = 0; i  <=10; i ++) {
+//    if (i === 5) {continue;}
+//    console.log(i);
+// }
+
+// for (let i  = 0; i  <=10; i ++) {
+//        if (i === 7) {break;}
+//        console.log(i);
+//     }
