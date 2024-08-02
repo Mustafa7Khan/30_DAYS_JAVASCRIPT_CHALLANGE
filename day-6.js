323d71e5a45bad1c484e47e2515141bd0dbd0bb2
Mustafa khan <94809609+Mustafa7Khan@users.noreply.github.com>
2024-08-02 15:22:22 +0500

    Add files via upload
    Arrays

---
+++ day-6.js
@@ -0,0 +1,63 @@
+// Arrays
+
+// 01 : Array Creation and Access
+
+// const arr = [ 1, 2, 3, 4, 5];
+// console.log(arr);
+// console.log(arr[0],arr[1]);
+
+
+// 02 : Array Methods(Basic)
+
+// const arr = [ 1, 2, 3, 4, 5];
+// arr.push(6);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.unshift(7);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+// 03 : Array Methods (Intermediate)
+
+// const arr = [ 1, 2, 3, 4, 5];
+// const result = arr.map((i)=> {
+//     return i*2;
+// })
+// console.log(result);
+
+// const result = arr.filter((i)=> {
+//     return i %2 ==0;
+// });
+// console.log(result);
+
+// let inital = 0
+// const result = arr.reduce(
+//     (accumulator,curr) => 
+//     accumulator + curr,
+//     inital ,
+// );
+// console.log(result);
+
+// Array Iteration
+
+// const arr = [ 1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element)
+// }
+
+// const result = arr.forEach((i)=> {
+//     console.log(i)
+// })
+
+// 05 : Multi-dimensional Arrays
+
+// const MultiDimeArray = [
+//     [1,2,3,4,5],
+//     [6,7,8,9,10]
+// ] 
+// console.log(MultiDimeArray.flat());
+
+// console.log(MultiDimeArray[1][4]);
