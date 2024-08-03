9bd09fe286484006d2a2106728fc62515f6cb24f
Mustafa khan <94809609+Mustafa7Khan@users.noreply.github.com>
2024-08-04 03:10:47 +0500

    Add files via upload
    DOM Manipulation

---
+++ day-9.js
@@ -0,0 +1,47 @@
+// DOM Manipulation
+
+// 01 Selecting and Manipulating Elements
+
+// const demo = document.getElementById("demo");
+
+// const demo2 = document.getElementsByClassName("demo2");
+
+// 02 : Creating and Appending Elements 
+
+// const div = document.createElement("div");
+// div.textContent = "Welcome div";
+
+// const unorderList = document.getElementsByTagName("ul");
+// const new_li = document.createElement("li");
+// element.appendChild(new_li)
+
+// 03 : Removing Elements
+
+// const element = document.getElementsByTagName("p");
+
+// element.remove();
+// element.parentNode.removeChild(element);
+
+// unorderList.removeChild(new_li);
+
+
+// 04 : Modifying Attributes and Classes
+
+// const element = document.getElementsByTagName("p");
+// element.style.color = "white";
+// element.removeProperty("color")
+
+// const img = document.getElementsByTagName("img");
+// img.setAttribute("src", "new_img.png");
+
+// 05 : Event Handling
+
+// const element = document.getElementsByTagName("p");
+// const btn = document.getElementsByTagName("button");
+// btn.addEventListener('click', event => {
+//     element.textContent ="p updated";
+// });
+
+// btn.addEventListener("mouseover", event => {
+//     element.style.borderColor = "red);
+// })
